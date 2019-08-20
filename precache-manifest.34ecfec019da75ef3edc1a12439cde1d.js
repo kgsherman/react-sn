8eb4a4a177b2edd9b6d114e7e120f0b1c5e8fbea
Kevin Sherman <sherman.kevin.g@gmail.com>
2019-08-20 12:00:14 +0100

    Updates

---
+++ precache-manifest.34ecfec019da75ef3edc1a12439cde1d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49ce51e4fed439a6a6fb365e63f3454d",
+    "url": "/react-sn/index.html"
+  },
+  {
+    "revision": "40b5aeab2e7b49f83f90",
+    "url": "/react-sn/static/js/2.932054db.chunk.js"
+  },
+  {
+    "revision": "d3a9646bf953ec954284",
+    "url": "/react-sn/static/js/main.c37732c9.chunk.js"
+  },
+  {
+    "revision": "00bff44151778e89524c",
+    "url": "/react-sn/static/js/runtime~main.a231af44.js"
+  }
+]);
